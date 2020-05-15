63511681666be86973871b01cf9a73759033a67a
sriastutikaryawati <64571694+sriastutikaryawati@users.noreply.github.com>
2020-05-15 07:37:33 +0700

    Add files via upload
    program menggunakan bahasa C

---
+++ jobsheet 2 C.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+main (){
+	printf("nama: sri astuti karyawati\n");
+	printf("nim: F1B019133\n");
+	printf("kelompok: 24\n");
+int nim[10]= {1, 3, 3};
+
+printf ("Nim ke-1 = %i\n",nim[0]);
+printf ("Nim ke-2 = %i\n",nim[1]);
+printf ("Nim ke-3 = %i\n",nim[2]);
+
+}
