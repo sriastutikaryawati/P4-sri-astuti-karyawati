220c7bed87aa535c8ae5d316b746dea12d6425d8
sriastutikaryawati <64571694+sriastutikaryawati@users.noreply.github.com>
2020-05-15 07:38:50 +0700

    Add files via upload
    program menggunakan bahasa C++

---
+++ jobsheet 2 C++.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+int main (){
+	cout<<("Nama : sri astuti karyawati\n");
+	cout<<("Nim : 133\n");
+	cout<<("Kelompok : 24\n\n");
+	
+	int bilangan[2][2];
+	bilangan[0][0]=133;
+	bilangan[0][1]=120;
+	
+	cout<<("akhiran nim dua mahasiswa:\n");
+	cout<<(" nim ke-1 : \n",bilangan[0][0])<<endl;
+	cout<<(" nim ke-2 : \n",bilangan[0][1])<<endl;
+
+	
+	return 0;
+
+	}
+
