c03341653838d0df902b85ab9d830782a0dfeb17
sriastutikaryawati <64571694+sriastutikaryawati@users.noreply.github.com>
2020-05-15 07:36:16 +0700

    Add files via upload
    program menggunakan bahasa C++

---
+++ jobsheet 1 C++.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+main(){
+	cout<<"nama :sri astuti karyawati\n"<<endl;
+	cout<<"\nnim :133"<<endl;
+	cout<<"\nkelompok :24\n"<<endl;
+	int my_array[3]= {1,3,3};
+	cout<<("nim ke-1 = ",my_array[0]);
+	cout<<("nim ke-2 = ",my_array[1]);
+	cout<<("nim ke-3 = ",my_array[2]);
+	
+}
